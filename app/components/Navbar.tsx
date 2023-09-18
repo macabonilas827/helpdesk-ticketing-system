dfb3e69ee832fbb284229e6d1912f7d6a5e414a0
Mark Anel Cabonilas <macabonilas827@gmail.com>
2023-09-18 09:11:07 +0800

    add links to dashboard and tickets

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,7 +1,11 @@
+import Link from "next/link"
+
 export default function Navbar() {
   return (
     <nav>
       <h1>HTS System</h1>
+      <Link href="/">Dashboard</Link>
+      <Link href="/tickets">Tickets</Link>
     </nav>
   )
 }
